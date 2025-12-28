d550acf635c9c372a0c5b3be6212b20954ab3a7f
aleksi-svaridze <aleksi.svaridze82@gmail.com>
2025-12-29 01:03:41 +0400

    0.0.2

--- src/main.js
+++ src/main.js
@@ -2,9 +2,21 @@ const hoursContainer = document.querySelector("#hours");
 const minutesContainer = document.querySelector("#minutes");
 const secondsContainer = document.querySelector("#seconds");
 
+function updateClock() {
+  const currentTime = new Date();
+
+  hoursContainer.innerHTML = currentTime.getHours().toString().padStart(2, "0");
+  minutesContainer.innerHTML = currentTime
+    .getMinutes()
+    .toString()
+    .padStart(2, "0");
+  secondsContainer.innerHTML = currentTime
+    .getSeconds()
+    .toString()
+    .padStart(2, "0");
+}
+
 setInterval(() => {
-  const now = new Date();
-  hoursContainer.innerHTML = now.getHours().toString().padStart(2, "0");
-  minutesContainer.innerHTML = now.getMinutes().toString().padStart(2, "0");
-  secondsContainer.innerHTML = now.getSeconds().toString().padStart(2, "0");
+  updateClock;
 }, 1000);
+updateClock();
